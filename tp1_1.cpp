8a3a021e4044b6a68b309b7051178f9fb1d60f66
Gonzalo <gonzalopez723@gmail.com>
2024-04-07 22:59:14 -0300

    actualizacion tp1_1.cpp

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -4,4 +4,14 @@ int main() {
 
     printf("hola mundo\n");
 
+    int variable = 10;
+    int *puntero = &variable;
+
+    printf("Contenido del puntero: %d\n", *puntero);
+    printf("Direccion de memoria almacenada por el puntero: %d\n", puntero);
+    printf("Direccion de memoria de la variable: %d\n", &variable);
+    printf("Direccion de memoria del puntero: %d\n", &puntero);
+    printf("Tamano de memoria usado por la variable: %d\n", sizeof(variable));
+
+    return 0;
 }
